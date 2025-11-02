713c1ba3151be89d29f01e2348b0469050148baa
felipe d ferreira <felipedferreira11@gmail.com>
2025-11-02 01:43:38 -0500

    chore: adds validation to request object to creating a movie

--- src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieRequest.cs
+++ src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieRequest.cs
@@ -1,4 +1,19 @@
-﻿namespace Prometheus.MovieBuff.Presentation.Features.Movies.CreateMovie.v1;
+﻿using System.ComponentModel.DataAnnotations;
 
-// consider using a class with data annotations for validation
-public record CreateMovieRequest(string Title, int YearOfRelease, IEnumerable<string> Genres);
+namespace Prometheus.MovieBuff.Presentation.Features.Movies.CreateMovie.v1;
+
+public class CreateMovieRequest
+{
+    [Required]
+    [MinLength(1)]
+    [MaxLength(250)]
+    public string Title { get; set; }
+    
+    [Required]
+    [Range(1_950, 3_000, ErrorMessage = "Year of release must be after 1950 but before 3000.")]
+    public int YearOfRelease { get; set; }
+
+    [Required(ErrorMessage = "At least one genre must be specified.")]
+    [MinLength(1, ErrorMessage = "At least one genre must be specified.")]
+    public IEnumerable<string> Genres { get; set; } = [];
+}
